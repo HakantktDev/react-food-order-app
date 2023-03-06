02ae0fdcfc4ce3e88276d10d33023e015f56caa1
Hakan Tokat <hakantokatdev@gmail.com>
2023-03-06 15:45:09 +0300

    handling-loading-state

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-1c3b2-default-rtdb.firebaseio.com/meals.json');
@@ -21,10 +22,19 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes['meals-loading']}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
